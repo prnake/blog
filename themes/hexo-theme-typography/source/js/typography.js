3250c0ffd853c880b126c35b9a22c9f582e5ab5a
papersnake <prnake@gmail.com>
2023-01-18 11:47:13 +0800

    update(theme): fix speed issue

--- themes/hexo-theme-typography/source/js/typography.js
+++ themes/hexo-theme-typography/source/js/typography.js
@@ -24,7 +24,7 @@ function updateSidebar() {
         var sidebarW =
             stage.width() - $('#main-container').outerWidth() + (window.innerWidth - stage.innerWidth()) / 2;
         $('#side-bar').outerWidth(sidebarW);
-        console.log("sidebarW=" + sidebarW);
+        // console.log("sidebarW=" + sidebarW);
     }
 }
 $(document).ready(function () {
